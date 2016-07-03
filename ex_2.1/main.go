e8bb8ebcb88eb5b3e8b49290330e2b31dc879bab
salamandra19 <tatyana.krivonos@gmail.com>
2016-07-03 11:06:53 +0300

    °C °F °K

---
+++ ex_2.1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/salamandra19/ex/ex_2.1/tempconv"
+)
+
+func main() {
+	fmt.Println(tempconv.CToF(tempconv.BoilingC))
+	fmt.Println(tempconv.KToC(tempconv.BoilingK))
+}
