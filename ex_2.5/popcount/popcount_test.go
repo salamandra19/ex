cbc65a5b6fba4557a15792364d2c318d4f114a12
salamandra19 <tatyana.krivonos@gmail.com>
2016-07-03 20:14:40 +0300

    base 2.5

---
+++ ex_2.5/popcount/popcount_test.go
@@ -0,0 +1,50 @@
+package popcount
+
+import "testing"
+
+func TestPopCount(t *testing.T) {
+	cases := []struct {
+		x    uint64
+		want int
+	}{
+		{0, 0},
+		{5, 2},
+		{12, 2},
+		{0xFFF000, 12},
+		{^uint64(0), 64},
+	}
+	for _, v := range cases {
+		got := PopCount(v.x)
+		if got != v.want {
+			t.Errorf("PopCount(%d) = %d, want %d", v.x, got, v.want)
+		}
+
+		got = PopCountLoop(v.x)
+		if got != v.want {
+			t.Errorf("PopCountLoop(%d) = %d, want %d", v.x, got, v.want)
+		}
+
+		got = PopCountEach(v.x)
+		if got != v.want {
+			t.Errorf("PopCountEach(%d) = %d, want %d", v.x, got, v.want)
+		}
+	}
+}
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount(0x1122334455667788)
+	}
+}
+
+func BenchmarkPopCountLoop(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountLoop(0x1122334455667788)
+	}
+}
+
+func BenchmarkPopCountEach(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountEach(0x1122334455667788)
+	}
+}
