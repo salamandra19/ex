0578ffb229e6e77f58784dfb266d7c19a2337084
salamandra19 <tatyana.krivonos@gmail.com>
2016-07-09 09:53:51 +0300

    change to bytes.Buffer

--- ex_3.10/main.go
+++ ex_3.10/main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 )
 
 func main() {
-	for i := 1; i < len(os.Args); i++ {
-		fmt.Printf(" %s\n", comma(os.Args[i]))
+	for _, s := range os.Args[1:] {
+		fmt.Println(comma(s))
 	}
 }
 
 func comma(s string) string {
-	n := len(s)
-	if n <= 3 {
-		return s
+	var b bytes.Buffer
+	x := len(s) % 3
+	if x != 0 {
+		fmt.Fprintf(&b, "%s", s[:x])
 	}
-	return comma(s[:n-3]) + "," + s[n-3:]
+	s = s[x:]
+	for len(s) > 0 {
+		if b.Len() > 0 {
+			b.WriteString(",")
+		}
+		fmt.Fprintf(&b, "%s", s[:3])
+		s = s[3:]
+	}
+	return b.String()
 }
