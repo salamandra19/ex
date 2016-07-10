29a6312da4e6979fb1db0d77b499d9b171838b46
salamandra19 <tatyana.krivonos@gmail.com>
2016-07-10 10:21:55 +0300

    comma + comma1

--- ex_3.10/main.go
+++ ex_3.10/main.go
@@ -8,26 +8,37 @@ import (
 
 func main() {
 	for _, s := range os.Args[1:] {
-		fmt.Println(comma(s))
+		fmt.Println(comma1(s))
 	}
 }
 
 func comma(s string) string {
 	var b bytes.Buffer
 	x := len(s) % 3
-	if x == 0 {
-		x = 3
+	if x != 0 {
+		fmt.Fprintf(&b, "%s", s[:x])
 	}
-	for i := x; len(s) > 0; {
-		if len(s) > 3 {
-			fmt.Fprintf(&b, "%s", s[:i])
+	s = s[x:]
+	for len(s) > 0 {
+		if b.Len() > 0 {
 			b.WriteString(",")
-			s = s[i:]
-			i = 3
-		} else {
-			b.WriteString((s[:i]))
-			break
 		}
+		fmt.Fprintf(&b, "%s", s[:3])
+		s = s[3:]
 	}
+	return b.String()
+}
+
+func comma1(s string) string {
+	var b bytes.Buffer
+	for i := len(s) % 3; len(s) > 3; i = 3 {
+		if i == 0 {
+			i = 3
+		}
+		b.WriteString(s[:i])
+		b.WriteString(",")
+		s = s[i:]
+	}
+	b.WriteString(s)
 	return b.String()
 }
