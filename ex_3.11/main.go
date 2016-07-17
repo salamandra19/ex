0385049e067a217d436c1bc4b2b258145ada13ab
salamandra19 <tatyana.krivonos@gmail.com>
2016-07-17 16:02:19 +0300

    cleaned

--- ex_3.11/main.go
+++ ex_3.11/main.go
@@ -11,23 +11,21 @@ func main() {
 		fmt.Println(detail(s))
 	}
 }
+
 func detail(s string) string {
 	var rest, sign string
 	if len(s) > 0 && (s[0] == '-' || s[0] == '+') {
-		sign = (string(s[0]))
-		s = s[1:]
+		sign, s = string(s[0]), s[1:]
 	}
 	for i := len(s) - 1; i >= 0; i-- {
 		if s[i] == '.' {
-			rest = s[i:]
-			s = s[:i]
+			rest, s = s[i:], s[:i]
 			break
 		}
 	}
-	s = sign + comma(s) + rest
-
-	return s
+	return sign + comma(s) + rest
 }
+
 func comma(s string) string {
 	var b bytes.Buffer
 	for start, end := 0, len(s)%3; end <= len(s); start, end = end, end+3 {
