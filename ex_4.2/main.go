a5dd50e4974e7db9c48c4d09e4ca4c23254ed9b4
salamandra19 <tatyana.krivonos@gmail.com>
2016-08-01 15:36:32 +0300

    os.Stdin

--- ex_4.2/main.go
+++ ex_4.2/main.go
@@ -5,22 +5,35 @@ import (
 	"crypto/sha512"
 	"flag"
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 var f = flag.Int("algo", 256, "SHA алгоритм, допустимые значения 256, 384, 512")
 
 func main() {
 	flag.Parse()
-	for _, data := range flag.Args() {
-		switch *f {
-		case 256:
-			fmt.Printf("%x\n", sha256.Sum256([]byte(data)))
-		case 384:
-			fmt.Printf("%x\n", sha512.Sum384([]byte(data)))
-		case 512:
-			fmt.Printf("%x\n", sha512.Sum512([]byte(data)))
-		default:
-			fmt.Println("incorrect algo, expect 256/384/512")
+	switch *f {
+	case 256:
+		h := sha256.New()
+		if _, err := io.Copy(h, os.Stdin); err != nil {
+			log.Fatal(err)
 		}
+		fmt.Printf("%x\n", h.Sum(nil))
+	case 384:
+		h := sha512.New384()
+		if _, err := io.Copy(h, os.Stdin); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%x\n", h.Sum(nil))
+	case 512:
+		h := sha512.New()
+		if _, err := io.Copy(h, os.Stdin); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%x\n", h.Sum(nil))
+	default:
+		fmt.Println("incorrect algo, expect 256/384/512")
 	}
 }
