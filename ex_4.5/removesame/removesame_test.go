6c99e3e73a947919fa7ad45c1110cc37b8e12f5f
salamandra19 <tatyana.krivonos@gmail.com>
2016-08-10 23:32:53 +0300

    added cases

--- ex_4.5/removesame/removesame_test.go
+++ ex_4.5/removesame/removesame_test.go
@@ -12,12 +12,15 @@ func TestRemoveSame(t *testing.T) {
 	}{
 		{[]string{"aaa"}, []string{"aaa"}},
 		{[]string{"a", "a", "a", "b"}, []string{"a", "b"}},
-		{[]string{"b"}, []string{"b"}},
+		{[]string{"bb", "bb"}, []string{"bb"}},
+		{[]string{"bb", "aa", "bb", "aa"}, []string{"bb", "aa", "bb", "aa"}},
+		{[]string{"bb", "bb", "aa", "aa"}, []string{"bb", "aa"}},
+		{[]string{"aaa", "bbb", "ccc", "ccc", "ccc", "ddd", "ddd", "ddd", "ddd", "ccc", "bbb"}, []string{"aaa", "bbb", "ccc", "ddd", "ccc", "bbb"}},
 		{[]string{}, []string{}},
 	}
 	for _, v := range tests {
 		got := RemoveSame(v.a)
-		if reflect.DeepEqual(got, v.want) == false {
+		if !reflect.DeepEqual(got, v.want) {
 			t.Errorf("RemoveSame %v = %v want %v", v.a, got, v.want)
 		}
 	}
