e096fa7ff8c0fb927ff4652b24c999c6c605eb67
salamandra19 <tatyana.krivonos@gmail.com>
2017-10-02 14:46:40 +0300

    fixed

--- sha/sha.go
+++ sha/sha.go
@@ -1,9 +1,17 @@
 package sha
 
-func Sha(a, b [32]uint8) int {
-	var s int
-	for i := 0; i < 32; i++ {
-		s = int(a[i] ^ b[i])
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
 	}
-	return s
+}
+
+func Sha(a, b [32]byte) int {
+	sum := 0
+	for i := 0; i < len(a); i++ {
+		sum += int(pc[(a[i] ^ b[i])])
+	}
+	return sum
 }
