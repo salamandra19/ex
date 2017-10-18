70358b1d644e1f9a6c356bcc96a76bfb2608e6e7
salamandra19 <tatyana.krivonos@gmail.com>
2017-10-18 09:55:22 +0300

    correct

--- ex_4.9/main.go
+++ ex_4.9/main.go
@@ -3,27 +3,29 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"log"
 	"os"
 )
 
 func main() {
 	counts := make(map[string]int)
-	file := os.Args[1:]
-	for _, arg := range file {
+	files := os.Args[1:]
+	for _, arg := range files {
 		f, err := os.Open(arg)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "wordfreq: %v\n", err)
+			log.Print(err)
 			continue
 		}
 		wordfreq(f, counts)
 		f.Close()
 	}
-	for line, n := range counts {
-		fmt.Printf("%s\t%d\n", line, n)
+	for word, n := range counts {
+		fmt.Printf("%s\t%d\n", word, n)
 	}
 }
 
-func wordfreq(f *os.File, counts map[string]int) {
+func wordfreq(f io.Reader, counts map[string]int) {
 	input := bufio.NewScanner(f)
 	input.Split(bufio.ScanWords)
 	for input.Scan() {
