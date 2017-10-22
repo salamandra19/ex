a7aab1e30366b8ecea5ea865c06bcae6d05237bd
salamandra19 <tatyana.krivonos@gmail.com>
2017-10-22 14:32:22 +0300

    clean

--- ex_4.10/main.go
+++ ex_4.10/main.go
@@ -10,37 +10,38 @@ import (
 )
 
 func main() {
-	var less_then_month []*github.Issue
-	var less_then_year []*github.Issue
-	var more_then_year []*github.Issue
 	result, err := github.SearchIssues(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("%d тем:\n", result.TotalCount)
+
+	var less_than_month []*github.Issue
+	var less_than_year []*github.Issue
+	var more_than_year []*github.Issue
+
 	t := time.Now()
 	month := t.AddDate(0, -1, 0)
 	year := t.AddDate(-1, 0, 0)
 	for _, item := range result.Items {
 		switch {
 		case item.CreatedAt.After(month):
-			less_then_month = append(less_then_month, item)
+			less_than_month = append(less_than_month, item)
 		case item.CreatedAt.After(year):
-			less_then_year = append(less_then_year, item)
-		case item.CreatedAt.Before(year):
-			more_then_year = append(more_then_year, item)
+			less_than_year = append(less_than_year, item)
+		default:
+			more_than_year = append(more_than_year, item)
 		}
 	}
-	fmt.Println("менее месяца:")
-	for _, item := range less_then_month {
-		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
-	}
-	fmt.Println("менее года:")
-	for _, item := range less_then_year {
-		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
-	}
-	fmt.Println("более года:")
-	for _, item := range more_then_year {
+
+	printIssue("менее месяца:", less_than_month)
+	printIssue("менее года:", less_than_year)
+	printIssue("более года:", more_than_year)
+}
+
+func printIssue(s string, period []*github.Issue) {
+	fmt.Println(s)
+	for _, item := range period {
 		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
 	}
 }
